e2d7c44c35032997036fe8d175d16e69891279d6
Joseph-Britt <48034902+Joseph-Britt@users.noreply.github.com>
2024-07-21 18:52:49 -0500

    Main Menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
-        
+
+
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button quitButton;
+    [SerializeField] private string gameScene;
+    [SerializeField] private GameObject creditsMenu;
+
+
+    private void Awake() {
+        startButton.onClick.AddListener(() => {
+            SceneManager.LoadScene(gameScene);
+        });
+        creditsButton.onClick.AddListener(() => {
+            ToggleCredits();
+        });
+        quitButton.onClick.AddListener(() => {
+            Application.Quit();
+        });
     }
 
-    // Update is called once per frame
-    void Update() {
-        
+    public void ToggleCredits() {
+        creditsMenu.SetActive(!creditsMenu.activeSelf);
+        gameObject.SetActive(!creditsMenu.activeSelf);
     }
+
 }
